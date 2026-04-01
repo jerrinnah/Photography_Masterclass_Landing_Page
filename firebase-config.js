d8335f0df08e7062d96d9714a2847a0c50802f36
Jerry Nnah <devjerrynnah@gmail.com>
2026-04-01 03:28:29 +0100

    firebase update

--- firebase-config.js
+++ firebase-config.js
@@ -4,13 +4,23 @@
 // Project Settings → General → Your apps → SDK setup
 // ═══════════════════════════════════════════════════════
 
+// const firebaseConfig = {
+//   apiKey:            "YOUR_API_KEY",
+//   authDomain:        "YOUR_PROJECT_ID.firebaseapp.com",
+//   projectId:         "YOUR_PROJECT_ID",
+//   storageBucket:     "YOUR_PROJECT_ID.appspot.com",
+//   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+//   appId:             "YOUR_APP_ID"
+// };
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey:            "YOUR_API_KEY",
-  authDomain:        "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId:         "YOUR_PROJECT_ID",
-  storageBucket:     "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId:             "YOUR_APP_ID"
+  apiKey: "AIzaSyCC2BBlbG6HgX48ozJGuh9v2WsTkMTtUrA",
+  authDomain: "masterclass-6cf9e.firebaseapp.com",
+  projectId: "masterclass-6cf9e",
+  storageBucket: "masterclass-6cf9e.firebasestorage.app",
+  messagingSenderId: "423647132415",
+  appId: "1:423647132415:web:cb8ecc129efecbe1dbeaca",
+  measurementId: "G-S832XE2ZYE",
 };
 
 export default firebaseConfig;
